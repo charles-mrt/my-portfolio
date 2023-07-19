e2ee38aa2e04cb995d36fa51e422bd23b3e6859d
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-19 15:54:44 -0300

    added header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,20 @@
 
 
-import { Menu } from './components/Menu/Menu';
 import { FooterInfo } from './components/FooterInfo';
 import { WaveDivider } from './components/WaveDivider';
 import charlesLogo from '/public/assets/logo.svg';
+
+import { Header } from './components/Header';
 
 import { Section as Section1 } from './components/Sections/Section1/Section';
 import { Section as Section2 } from './components/Sections/Section2/Section';
 import { Section as Section3 } from './components/Sections/Section3/Section';
 
-
 export default function Home() {
+
   return (
     <div className="min-h-screen flex flex-col">
-
-      <header className="h-10 w-screen py-2 px-6 flex justify-between items-center bg-transparent fixed z-50">
-        <Menu imageUrl={charlesLogo} />
-      </header>
+      <Header />
 
       <div className="flex flex-col flex-grow">
         <main className="flex-grow">
@@ -28,7 +26,7 @@ export default function Home() {
         <div className="w-full bg-zinc-900 bg-opacity-90">
           <WaveDivider bgTextColor="text-zinc-700" />
           <footer className="h-10 w-full p-2 bg-zinc-700 relative flex items-center justify-between">
-            <FooterInfo imageUrl={charlesLogo}/>
+            <FooterInfo imageUrl={charlesLogo} />
           </footer>
         </div>
       </div>
