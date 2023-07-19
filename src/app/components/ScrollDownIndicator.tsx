eb61047c1b82073be47aecd741269afb244dab72
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-19 11:39:14 -0300

    added padding top and bottom
    Signed-off-by: charles-mrt <charlesmartins.ads@gmail.com>

--- src/app/components/ScrollDownIndicator.tsx
+++ src/app/components/ScrollDownIndicator.tsx
@@ -7,7 +7,7 @@ import {
 
 export const ScrollDownIndicator = () => {
   return (
-    < div className="text-green-400 mt-8 items-center flex flex-col " >
+    < div className="text-green-400 mt-8 items-center flex flex-col py-4">
       <Mouse size={31} />
       <Triangle size={28} className="transform rotate-180 mt-2 opacity-60" />
       <Triangle size={26} className="transform rotate-180 -mt-3.5 opacity-60" />
