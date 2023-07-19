8a100e4b491cc3891b54f37e8843fcf923e8b152
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-19 15:56:44 -0300

    Added color props for the componen MenuPropst

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -1,29 +1,27 @@
 
-
 import Image from 'next/image'
 import { MenuLink } from './MenuNav';
 import { SocialMediaIcons } from '../SocialMediaIcons';
 
 interface MenuProps {
-  imageUrl: string,
+  imageUrl: string;
+  color: string;
 }
+export const Menu = ({ imageUrl, color}: MenuProps) => {
 
-export const Menu = ({imageUrl}:MenuProps) => {
   return (
     <>
       <div className="w-[33%]">
         <Image src={imageUrl} alt="charles portfolio logo" />
       </div>
 
-     
       <nav className=" flex justify-center w-auto gap-6">
         < MenuLink linkText="sobre mim" textAnchor="about-me" />
         < MenuLink linkText="projetos" textAnchor="project" />
         < MenuLink linkText="contato" textAnchor="contact" />
       </nav>
 
-      {/* <Menu className="text-zinc-50" /> */}
-      <div className="w-[33%] flex justify-end gap-5 text-zinc-900">
+      <div className={`w-[33%] flex justify-end gap-5 ${color}`}>
         <SocialMediaIcons />
       </div>
     </>
