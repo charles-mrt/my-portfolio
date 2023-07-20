acdbfd19ccda805cf8e7b7a4d2fdc0f657c15bf3
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-20 14:59:49 -0300

    added hover effect for scroll indicator and removed unnecessary jsx

--- src/app/components/ScrollDownIndicator.tsx
+++ src/app/components/ScrollDownIndicator.tsx
@@ -4,14 +4,22 @@ import {
   Triangle,
 } from 'lucide-react';
 
+interface ScrollDownIndicatorProps {
+  idName?: string;
+}
 
-export const ScrollDownIndicator = () => {
+export const ScrollDownIndicator = ({ idName }: ScrollDownIndicatorProps) => {
+
   return (
-    < div className="text-green-400 mt-8 items-center flex flex-col py-4">
-      <Mouse size={31} />
-      <Triangle size={28} className="transform rotate-180 mt-2 opacity-60" />
-      <Triangle size={26} className="transform rotate-180 -mt-3.5 opacity-60" />
-      <Triangle size={24} className="transform rotate-180 -mt-3 opacity-60" />
-    </div >
+
+    <a href={`#${idName}`}
+      className="text-green-700 mt-8 items-center flex flex-col py-4 ease-linear duration-300 hover:text-green-400"
+    >
+      <Mouse size={31} className="text-green-400" />
+      <Triangle size={28} className="transform rotate-180 mt-2 opacity-90" />
+      <Triangle size={26} className="transform rotate-180 -mt-3.5 opacity-80" />
+      <Triangle size={24} className="transform rotate-180 -mt-3 opacity-75" />
+    </a>
+    
   )
 }
