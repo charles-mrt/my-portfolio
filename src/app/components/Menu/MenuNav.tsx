f0f466f2e972f3f3d779de3e4e3c1ba581162574
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-20 16:16:56 -0300

    added icon props, to add  icon before menu item name, coded component style to receive the icon adn rename component MenuNav

--- src/app/components/Menu/MenuNav.tsx
+++ src/app/components/Menu/MenuNav.tsx
@@ -1,16 +1,18 @@
+import { ReactElement } from "react";
 
-interface MenuLinkProps {
+interface MenuNavProps {
+  icon?: ReactElement;
   linkText: string;
-  textAnchor:string;
+  textAnchor: string;
 }
 
-export const MenuLink = ({ linkText, textAnchor }: MenuLinkProps) => {
+export const MenuNav = ({ linkText, icon, textAnchor }: MenuNavProps) => {
   return (
     <a
-     className="text-zinc-50 text-base font-bold uppercase border-b-4 border-transparent ease-linear duration-300 hover:border-green-400" 
-     href={`#${textAnchor}`} 
+      className="text-zinc-50 text-base font-bold uppercase flex items-center gap-2 border-b-4 border-transparent ease-linear duration-300 hover:border-green-400"
+      href={`#${textAnchor}`}
     >
-      {linkText}
+      {icon}{linkText}
     </a>
   )
 }
