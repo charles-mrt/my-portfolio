5c5c8685394c1505dc61357d0ff1e017250b28a5
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-20 14:19:57 -0300

    added hover properties

--- src/app/components/Menu/MenuNav.tsx
+++ src/app/components/Menu/MenuNav.tsx
@@ -7,7 +7,7 @@ interface MenuLinkProps {
 export const MenuLink = ({ linkText, textAnchor }: MenuLinkProps) => {
   return (
     <a
-     className="text-zinc-50 text-base font-medium uppercase" 
+     className="text-zinc-50 text-base font-bold uppercase border-b-4 border-transparent ease-linear duration-300 hover:border-green-400" 
      href={`#${textAnchor}`} 
     >
       {linkText}
--- src/app/components/SocialMediaIcons.tsx
+++ src/app/components/SocialMediaIcons.tsx
@@ -9,10 +9,29 @@ import {
 export const SocialMediaIcons = () => {
   return (
     <>
-      <a href="" title="instagram Charles"><Instagram /></a>
-      <a href="" title="facebook Charles"><Facebook /></a>
-      <a href="" title="linkedin Charles"><Linkedin /></a>
-      <a href="" title="github Charles"><Github /></a>
+      <a href="" title="instagram Charles"
+        className="ease-linear duration-200 hover:scale-125"
+      >
+        <Instagram />
+      </a>
+
+      <a href="" title="facebook Charles"
+        className="ease-linear duration-200 hover:scale-125"
+      >
+        <Facebook />
+      </a>
+
+      <a href="" title="linkedin Charles"
+        className="ease-linear duration-200 hover:scale-125"
+      >
+        <Linkedin />
+      </a>
+
+      <a href="" title="github Charles"
+        className="ease-linear duration-200 hover:scale-125"
+      >
+        <Github />
+      </a>
     </>
   )
 }
