c76a8870d20456e1b727fb3d691128214b6a6428
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-23 17:49:10 -0300

    added new props to handle with open and close modal trow tailwind att fixed or hidden

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,14 +1,21 @@
 import { ListSocialMediaIcon } from "./SocialIcons/ListSocialMediaIcon"
 
-import { XCircle } from 'lucide-react';
+import { XCircle } from 'lucide-react'
 
-export const Modal = () => {
+interface ModalProps {
+  display: string;
+  onClose: () => void;
+}
+
+export const Modal = ({ display,onClose }: ModalProps) => {
 
   return (
+    
+    <div className={`w-4/5 h-1/2 flex flex-col justify-around bg-zinc-800 border-2 border-green-400 rounded-lg p-10 ${ display } top-0 bottom-0 right-0 left-0 m-auto z-[9999]`}>
 
-    <div id="teste2" className="w-4/5 h-1/2 flex flex-col justify-around bg-zinc-800 border-2 border-green-400 rounded-lg p-10 fixed top-0 bottom-0 right-0 left-0 m-auto z-[9999]">
-
-      <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
+      <a onClick={onClose}>
+        <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
+      </a>
 
       <p className="text-center font-semibold text-2xl text-zinc-50">
         Acesse minhas redes sociais e entre em contato, será um prazer lhe receber
