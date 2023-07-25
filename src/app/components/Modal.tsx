77238267d2d30951cdcc17f9963cc659678c8257
charles-mrt <charlesmartins.ads@gmail.com>
2023-07-25 17:19:15 -0300

    Update Head component with viewport meta tag and favicon link

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -7,23 +7,26 @@ interface ModalProps {
   onClose: () => void;
 }
 
-export const Modal = ({ display,onClose }: ModalProps) => {
+export const Modal = ({ display, onClose }: ModalProps) => {
 
   return (
-    
-    <div className={`w-4/5 h-1/2 flex flex-col justify-around bg-zinc-800 border-2 border-green-400 rounded-lg p-10 ${ display } top-0 bottom-0 right-0 left-0 m-auto z-[9999]`}>
 
-      <a onClick={onClose}>
-        <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
-      </a>
+    <div className={`w-full h-full bg-black bg-opacity-90 ${display} top-0 bottom-0 right-0 left-0 m-auto z-[9999] flex items-center justify-center`}>
+      
+      <div className="relative max-w-lg h-1/2 flex flex-col justify-around bg-zinc-800 border-2 border-green-400 rounded-lg p-10">
+        <a onClick={onClose}>
+          <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
+        </a>
 
-      <p className="text-center font-semibold text-2xl text-zinc-50">
-        Acesse minhas redes sociais e entre em contato, será um prazer lhe receber
-      </p>
+        <p className="text-center font-semibold text-2xl text-zinc-50">
+          Fique à vontade para me contatar pelas redes sociais,<br></br> será um prazer receber sua mensagem!
+        </p>
 
-      <div className="flex center gap-6 justify-center text-zinc-50">
-        <ListSocialMediaIcon iconSize={40} selector="border-2 border-green-400 rounded-lg p-6" />
+        <div className="flex center gap-6 justify-center text-zinc-50">
+          <ListSocialMediaIcon iconSize={40} selector="border-2 border-green-400 rounded-lg p-6" />
+        </div>
       </div>
+
     </div>
 
   )
---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+    </>
+  )
+}
