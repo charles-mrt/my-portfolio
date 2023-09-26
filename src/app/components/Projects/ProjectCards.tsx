eb6e22cf8a177974cfa63327b252d8668a00a949
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-26 19:48:29 -0300

    feat: Image type handling and addition of 'Read More' button

--- src/app/components/Projects/ProjectCards.tsx
+++ src/app/components/Projects/ProjectCards.tsx
@@ -5,11 +5,14 @@ import { Button } from '../Button';
 
 import {
   FileCode2,
+  IterationCcw,
   MonitorSmartphone
 } from 'lucide-react';
 
+import { useState } from 'react';
+
 interface projectCardsProps {
-  imageUrl: StaticImageData;
+  imageUrl: string | StaticImageData;
   alt: string;
   title: string;
   description: string;
@@ -20,8 +23,26 @@ interface projectCardsProps {
 }
 
 export const ProjectCards = ({ imageUrl, alt, title, technologiesListImage, description, project_url, github_url, category }: projectCardsProps) => {
+
+  let imageUrlString = "";
+
+  if (typeof imageUrl === 'string') {
+    imageUrlString = imageUrl;
+  } else if (typeof imageUrl === 'object') {
+    imageUrlString = imageUrl.src;
+  }
+
+  const isJpg = imageUrlString.toLowerCase().endsWith('.jpg') || imageUrlString.toLowerCase().endsWith('.jpeg');
+  const isGif = imageUrlString.toLowerCase().endsWith('.gif');
+
+  const [readMore, isReadMore] = useState(false)
+
+  const handleReaMoreButton = (isActived: boolean) => {
+    isReadMore(isActived)
+  }
+
   return (
-    
+
     <div className="custom-fade-animation h-[31.2rem] w-80 sm-device:w-72 sm:w-[19rem] md:w-[22rem] lg:w-[19.5rem] xl:w-96 2xl:h-[40.6rem] 
     bg-zinc-900 border-2 border-zinc-700 rounded-[5px]
     position:block
@@ -34,69 +55,106 @@ export const ProjectCards = ({ imageUrl, alt, title, technologiesListImage, desc
     transition-transform duration-200 ease-linear
     transform-gpu will-change-transform 
     will-change-box-shadow"
-    data-category={category}
+      data-category={category}
+      id={`${readMore ? "flip-card" : ""}`}
     >
-      <header
-        className="w-full h-1/2  overflow-hidden"
-        style={{ clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 80%)' }}
-      >
-        <Image src={imageUrl} width={500} height={500} alt={alt} className="w-full object-cover" />
-      </header>
+      {!readMore ? (
+        <>
+          <header className="w-full h-[45%]  overflow-hidden flex items-center bg-zinc-800">
 
-      <div className="w-full  p-3 h-1/2 flex flex-col justify-between">
+            {isJpg && (
+              <Image src={imageUrl} alt={alt} className="w-full h-full object-cover" />
+            )}
+            {isGif && (
+              <img src={imageUrl as string} alt={alt} className="w-full h-full object-cover" />
+            )}
 
-        <h3 className="text-base font-bold text-green-400 text-center uppercase leading-none">
-          {title}
-        </h3>
+          </header>
 
-        <p className="text-base text-zinc-100 font-normal tracking-[1px] text-center text-ellipsis overflow-hidden line-clamp-3">
-          {description}
-        </p>
+          <div className="w-full h-[55%] p-3 flex flex-col justify-between ">
 
-        <div className="flex flex-col items-center laptop:items-start ">
-          <div className="flex gap-2 mb-1">
+            <h3 className="text-base font-bold text-green-400 text-center uppercase leading-none">
+              {title}
+            </h3>
 
-            {technologiesListImage.map((techImageUrl, index) => (
-              <div key={index} className="w-10 h-10 p-1 border-[1px] rounded-[5px] border-zinc-700 flex items-center justify-center">
-                <Image
-                  src={techImageUrl}
-                  alt="logo"
-                  className="w-7 h-7"                 
-                />
+            <p className="text-base text-zinc-100 font-normal tracking-[1px] text-center text-ellipsis overflow-hidden line-clamp-3">
+              {description}
+            </p>
+
+            <a onClick={() => handleReaMoreButton(true)} className="uppercase text-zinc-100 font-semibold text-right cursor-pointer italic hover:text-green-500">
+              ler mais
+            </a>
+
+            <div className="flex flex-col items-center laptop:items-start ">
+              <div className="flex gap-2 mb-1">
+
+                {technologiesListImage.map((techImageUrl, index) => (
+                  <div key={index} className="w-10 h-10 p-1 border-[1px] rounded-[5px] border-zinc-700 flex items-center justify-center">
+                    <Image
+                      src={techImageUrl}
+                      alt="logo"
+                      className="w-7 h-7"
+                    />
+                  </div>
+                ))}
+
               </div>
-            ))}
+
+              <span className="text-zinc-200">tecnologias utilizadas</span>
+            </div>
+
+            <footer className="flex gap-2 ">
+              <Button
+                link={project_url}
+                title="acessar app"
+                fontSize="text-xs"
+                size="w-60 h-14"
+                bgColor="bg-transparent"
+                borderColor="border-green-400"
+                icon={<MonitorSmartphone />}
+                openNewTab={true}
+              />
+
+              <Button
+                link={github_url}
+                title="acessar código"
+                fontSize="text-xs"
+                size="w-60 h-14"
+                bgColor="bg-transparent"
+                borderColor="border-green-400 "
+                icon={<FileCode2 />}
+                openNewTab={true}
+              />
+            </footer>
 
           </div>
+        </>
 
-          <span className="text-zinc-200">tecnologias utilizadas</span>
+      ) : (
+        
+        <div id="flip-card" className="p-10  h-full flex flex-col items-center gap-10 overflow-y-auto">
+          
+          <h4 className="text-base font-bold text-green-400 text-center uppercase leading-none">
+            {title}
+          </h4>
+
+          <p className="text-base text-zinc-100 font-normal tracking-[1px] text-center">
+            {description}
+          </p>
+
+          <Button
+            title='voltar'
+            icon={<IterationCcw />}
+            fontSize=''
+            size="w-60 h-14"
+            bgColor="bg-transparent"
+            borderColor="border-green-400"
+            onClickHandle={() => handleReaMoreButton(false)}
+          />
         </div>
+      )}
 
-        <footer className="flex gap-2 ">
-          <Button
-            link={project_url}
-            title="acessar app"
-            fontSize="text-xs"
-            size="w-60 h-14"
-            bgColor="bg-transparent"
-            borderColor="border-green-400 "
-            icon={<MonitorSmartphone />}
-            openNewTab={true}
-          />
 
-          <Button
-            link={github_url}
-            title="acessar código"
-            fontSize="text-xs"
-            size="w-60 h-14"
-            bgColor="bg-transparent"
-            borderColor="border-green-400 "
-            icon={<FileCode2 />}
-            openNewTab={true}
-          />
-        </footer>
-
-      </div>
-
-    </div>
+    </div >
   )
 }
