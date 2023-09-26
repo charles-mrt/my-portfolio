b5ba1e5281dd55c75f4790332dd930b23e5be52e
charles-mrt <charlesmartins.ads@gmail.com>
2023-09-26 19:31:55 -0300

    feat: updated project data

--- src/app/data.ts
+++ src/app/data.ts
@@ -15,15 +15,20 @@ import html from '/public/assets/technologies/html.svg';
 import figma from '/public/assets/technologies/figma.svg';
 
 
-
-import walk from '/public/assets/projects/walk.jpg'
-import planning_poker from '/public/assets/projects/walk.jpg'
+import vertigo_login from '/public/assets/projects/vertigo-login.jpg'
+import design_system from '/public/assets/projects/design-system.jpg'
+import let_me_ask from '/public/assets/projects/letmeask.jpg'
+import filter_category from '/public/assets/projects/toggle-filtro-categoria.jpg'
+import star_rating from '/public/assets/projects/star-rating.jpg'
+import sidebar_widget from '/public/assets/projects/sidebar-widget.jpg'
+import happy from '/public/assets/projects/happy.jpg'
+import podcast_time from '/public/assets/projects/podcast-time.jpg'
 
 export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcGcxYmh1dW05bTh1azFxcWNicWpxdjh1cW03MnNpOWZwNDJmbHFjbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/evHtzG1TLlQh9CSsHd/giphy.gif",
     alt: "walk landing page",
     title: "walk",
     tecnologies: [reactjs, cssmodule],
@@ -35,10 +40,10 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: planning_poker,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNjd5aXAxZ29ubXNsbjhxYzZtazV0aDdleDJraWR0NDhyNzRzNTNrbiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/WqEcA6aRIBXQGbR2Ne/giphy.gif",
     alt: "planning poker",
     title: "planning poker",
-    tecnologies: [reactjs, nextjs, typescript, tailwind, nodejs],
+    tecnologies: [reactjs, nextjs, typescript, tailwind, nodejs, figma],
     categories: ["todas", "reactjs", "nextjs", "typescript", "tailwind", "nodejs"],
     description: "Sistema para realizar estimativa de esforço de tempo, utilizando a técnica de gamificação do planning poker.",
     url: "https://github.com/charles-mrt/planning-poker-client",
@@ -47,22 +52,22 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ3FlM2owOW85cThiZ2tsb2wxeGF5dm94ZW5veHlhb3ZiNDl1YjhzbyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Xzl64JLv8e9GQhq1u0/giphy.gif",
     alt: "espaço conecta",
     title: "espaço conecta",
     tecnologies: [php, wordpress, html, javascript, css],
     categories: ["todas", "php", "wordpress", "html", "javascript", "css"],
-    description: "Tema wordpress focado em divulgação de infoprodutos",
+    description: "Tema wordpress projetado para o Espaço Conecta, tem como foco principal potencializar as estratégias de marketing digital de afiliados e empreendedores digitais. Construído com uma abordagem centrada na conversão, na experiência do usuário com um layout minimalista",
     url: "http://espacoconecta.com",
     github: "https://github.com/charles-mrt/espaco-conecta-theme"
   },
   {
-    is_public: true,
-    image: walk,
+    is_public: false,
+    image: "",
     alt: "portfolio",
     title: "portfolio",
-    tecnologies: [reactjs,nextjs, typescript, tailwind],
-    categories: ["todas", "reactjs","nextjs", "typescript", "tailwind"],
+    tecnologies: [reactjs, nextjs, typescript, tailwind],
+    categories: ["todas", "reactjs", "nextjs", "typescript", "tailwind"],
     description: "O meu portfólio é um projeto onde falo um pouco sobre minha pessoa e apresento as tecnologias que tenho focado meus estudo, e apresento alguns projetos que já desenvolvi",
     url: "https://github.com/charles-mrt/my-portfolio",
     github: "https://github.com/charles-mrt/my-portfolio"
@@ -70,31 +75,31 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExajFnazQ1MTMxc3IxZHM1d2xvZ2d3YWZ3aGhneGd4ajZiY2NiemMwdCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Syn4JbGEtyPx41Lh13/giphy.gif",
     alt: "nike lebron",
     title: "nike lebron landing page",
     tecnologies: [javascript, html, css, figma],
     categories: ["todas", "javascript", "html", "css", "figma"],
     description: "landing page de alta conversão para venda de tenis com interação entre os modelos de tenis apresentados",
-    url: "http://espacoconecta.com",
-    github: "https://github.com/charles-mrt/espaco-conecta-theme"
+    url: "https://github.com/charles-mrt/nike-lebron-landing-page",
+    github: "https://github.com/charles-mrt/nike-lebron-landing-page"
   },
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdG90em9qZzJybTNhY3cxZnhsMzIzdmw4d2l0ZGVmdXFlZm51cTV3OCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/DlfVS1XqiklbBjoh9c/giphy.gif",
     alt: "rocket contact",
     title: "rocket contact",
     tecnologies: [javascript, html, css, figma],
     categories: ["todas", "javascript", "html", "css", "figma"],
     description: "Rocket Contact é uma aplicação web usuários a capacidade de realizar buscas de contatos de forma rápida e eficaz.",
-    url: "https://github.com/charles-mrt/rocket-contact",
-    github: "https://charles-mrt.github.io/rocket-contact/"
+    url: "https://charles-mrt.github.io/rocket-contact/",
+    github: "https://github.com/charles-mrt/rocket-contact"
   },
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExanFrOWJxOGZiNTZwNG5ub2RrcW54MWd3eGltdDIydWVlcmNtY2U5ZyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/9zBSO8XdvjnKR9uogv/giphy.gif",
     alt: "rocket upload",
     title: "rocket upload",
     tecnologies: [javascript, html, css, figma],
@@ -106,19 +111,19 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdXdjMmUxOWZtZjFzMThvbzFuZjJpdnhycjBqYXY4dzczcXJ5ODBlaCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/8qOeNU706QD3M1vPYE/giphy.gif",
     alt: "rocket kanban",
     title: "rocket kanban",
     tecnologies: [javascript, html, css, figma],
     categories: ["todas", "javascript", "html", "css", "figma"],
     description: "Rocket Kanban é uma aplicação web que oferece uma plataforma visual e colaborativa para gerir projetos e tarefas utilizando a metodologia Kanban utilizando funcionalidade de arrastar e soltar.",
-    url: "https://charles-mrt.github.io/rocket-upload/",
+    url: "https://charles-mrt.github.io/rocket-kanban/",
     github: "https://github.com/charles-mrt/rocket-kanban"
   },
 
   {
     is_public: true,
-    image: walk,
+    image: vertigo_login,
     alt: "vertigo login",
     title: "vertigo login",
     tecnologies: [javascript, html, css, figma],
@@ -130,9 +135,9 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
-    alt: "currency converter",
-    title: "currency converter",
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYXFjZ3huOGNkdWRiYWZ5Zng2anRsNmJxdHE4bjQ4a25na3ZzbnB3NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/kIveFB6r6zelWoml93/giphy.gif",
+    alt: "tomorrow it will rain",
+    title: "tomorrow it will rain",
     tecnologies: [javascript, html, css, figma],
     categories: ["todas", "javascript", "html", "css", "figma"],
     description: "O aplicativo 'Amanhã Choverá' é uma aplicação de previsão do tempo que permite aos utilizadores obter atualizações em tempo real sobre as condições meteorológicas de uma cidade específica. Ao fornecer o nome de uma cidade, o aplicativo exibe a temperatura atual, condição do tempo e uma previsão de cinco dias. O aplicativo foi desenvolvido utilizando HTML, CSS e JavaScript e utiliza a API do OpenWeatherMap para buscar dados meteorológicos.",
@@ -142,9 +147,9 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
-    alt: "vertigo login",
-    title: "vertigo login",
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExdWQ4NWVtcmhhMWgwcnc2d3Fnb2ozYmlua2g5djdxcmJxMGxrNnhvYSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/AKBoWaNnsZr9gnNhhC/giphy.gif",
+    alt: "currency converter",
+    title: "currency converter",
     tecnologies: [javascript, html, css, figma],
     categories: ["todas", "javascript", "html", "css", "figma"],
     description: "Currency Converter é um aplicativo que converte valores de moeda, permitindo que você insira o valor desejado e escolha o tipo de moeda. O aplicativo foi desenvolvido utilizando HTML, CSS e JavaScript e consome dados da a API Exchange Rate",
@@ -154,7 +159,7 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzVwNGxjczY0eW1hdnI0Z2U4eGhpN3prbmdpajN0N3V4czFlbHVkcCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/BTmIskEkrvEHgKVQtv/giphy.gif",
     alt: "my grandma's clock",
     title: "my grandma's clock",
     tecnologies: [javascript, html, css],
@@ -166,7 +171,7 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHdzbjFkbHZ6ZXdod3dndzM3dmJnaG0yb3VkanlzNGdmeXp0NTc5eSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/mPyXtFO2XZ3gZSabf6/giphy.gif",
     alt: "rocketpay",
     title: "rocketpay",
     tecnologies: [javascript, html, css],
@@ -175,10 +180,10 @@ export const projectsData = [
     url: "http://rocket-pay-iota.vercel.app/",
     github: "https://github.com/charles-mrt/explorer-lab-creditcard"
   },
-  
+
   {
     is_public: true,
-    image: walk,
+    image: design_system,
     alt: "ignite lab design system",
     title: "ignite lab design system",
     tecnologies: [reactjs, typescript, css, figma],
@@ -190,7 +195,7 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: star_rating,
     alt: "star rating shortcode for Wordpress",
     title: "star rating shortcode for Wordpress",
     tecnologies: [php, wordpress],
@@ -202,7 +207,7 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDBnN2pocDdlZHV5dXNlaTlyMnByOTloeGZmdmNwY3FqNG94OXRsZCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1ZIE8oPCbTuQSLf8oI/giphy.gif",
     alt: "custom login wp",
     title: "custom login wp",
     tecnologies: [php, wordpress, javascript, css],
@@ -214,7 +219,7 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: filter_category,
     alt: "filtro de categorias para Wordpress",
     title: "toogle para filtro de categorias",
     tecnologies: [php, wordpress, javascript, css],
@@ -225,19 +230,20 @@ export const projectsData = [
   },
 
   {
-    is_public: true,
-    image: walk,
+    is_public: false,
+    image: "",
     alt: "badge do while",
     title: "badge do while",
     tecnologies: [javascript, html, css],
     categories: ["todas", "javascript", "html", "css"],
-    description: "Cartão de presenta para evento do while da rocketseat. O projeto utiliza a funcionalidade de light e dark theme.",    url: "https://github.com/charles-mrt/toogle-category-list-post-grid",
+    description: "Cartão de presenta para evento do while da rocketseat. O projeto utiliza a funcionalidade de light e dark theme.",
+    url:"https://github.com/charles-mrt/projeto-Badge-Do-While2021/deployments/github-pages",
     github: "https://github.com/charles-mrt/projeto-Badge-Do-While2021"
   },
 
   {
     is_public: true,
-    image: walk,
+    image: let_me_ask,
     alt: "let me ask",
     title: "let me ask",
     tecnologies: [reactjs, typescript, cssmodule],
@@ -249,7 +255,7 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: podcast_time,
     alt: "podcast time",
     title: "podcast time",
     tecnologies: [reactjs, nextjs, typescript, sass],
@@ -261,11 +267,11 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: happy,
     alt: "happy",
     title: "happy",
-    tecnologies: [reactjs,typescript, css, reactnative, nodejs],
-    categories: ["todas", "reactjs","typescript", "css", "react-native", "nodejs"],
+    tecnologies: [reactjs, typescript, css, reactnative, nodejs],
+    categories: ["todas", "reactjs", "typescript", "css", "react-native", "nodejs"],
     description: "Este projeto apresenta um sistema para registrar orfanatos, permitindo que as pessoas escolham facilmente orfanatos para visitar. Usando um mapa interativo, você pode explorar orfanatos na sua região, tornando o processo de localização e agendamento de visitas mais acessível e eficiente. Além disso, foi criado um aplicativo móvel para uma experiência ainda mais conveniente. Este projeto foi desenvolvido com  durante um evento da RocketSeat.",
     url: "https://github.com/charles-mrt/Happy-nlw3-rocket-seat",
     github: "https://github.com/charles-mrt/Happy-nlw3-rocket-seat"
@@ -273,18 +279,18 @@ export const projectsData = [
 
   {
     is_public: true,
-    image: walk,
+    image: sidebar_widget,
     alt: "sidebar widget",
     title: "sidebar widget",
-    tecnologies: [php,wordpress,html, css],
-    categories: ["todas", "php","wordpress","html", "css"],
-    description: "SideBar widget é uma feaute que adiciona uma sidebar no do thema Hello-Elementor. Por padrão o tema vem sem opção de sidebar.",
+    tecnologies: [php, wordpress, html, css],
+    categories: ["todas", "php", "wordpress", "html", "css"],
+    description: "SideBar widget é uma feature que adiciona uma sidebar no do thema Hello-Elementor. Por padrão o tema vem sem opção de sidebar.",
     url: "https://github.com/charles-mrt/wp-default-sidebar-hello-theme-child",
     github: "https://github.com/charles-mrt/wp-default-sidebar-hello-theme-child"
   },
   {
-    is_public: true,
-    image: walk,
+    is_public: false,
+    image: "",
     alt: "top gear template",
     title: "top gear template",
     tecnologies: [javascript, html, css],
