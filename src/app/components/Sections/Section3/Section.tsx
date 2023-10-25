a6cffd0b825cf91c9e7f67fcb1dd82beb34541e8
charles-mrt <charlesmartins.ads@gmail.com>
2023-10-25 17:12:11 -0300

    feat: implemented card for next project

--- src/app/components/Sections/Section3/Section.tsx
+++ src/app/components/Sections/Section3/Section.tsx
@@ -6,11 +6,13 @@ import { SectionTitle } from '../../SectionTitle';
 import { projectsData } from '@/app/data';
 import { SearchProject } from '../../SearchProject';
 import { useState } from 'react';
+import { SearchCode } from 'lucide-react';
+import { ListSocialMediaIcon } from '../../SocialIcons/ListSocialMediaIcon';
 
 export const Section = () => {
   const [selectedCategory, setSelectedCategory] = useState("todas");
 
-  const handleCategoryChange = (category:string) => {
+  const handleCategoryChange = (category: string) => {
     setSelectedCategory(category.trim());
     console.log("selectedCategory " + selectedCategory)
   };
@@ -25,9 +27,9 @@ export const Section = () => {
         <header className="text-center mt-10 mb-5">
           <SectionTitle titleName1="projetos" titleName2="realizados." />
         </header>
-        
+
         <SearchProject onCategoryChange={handleCategoryChange} />
-       
+
         {/*cards */}
         <div className="w-full">
 
@@ -53,6 +55,26 @@ export const Section = () => {
                 )
               })
             }
+
+            <li className="relative h-[31.2rem] w-80 sm-device:w-72 sm:w-[19rem] md:w-[22rem] lg:w-[19.5rem] xl:w-96 2xl:h-[40.6rem] 
+            bg-zinc-900 p-10 border-2 border-dashed border-green-400 rounded-[5px] flex flex-col justify-around items-center">
+
+              <header className="text-4xl text-center text-zinc-50">
+                Curioso para saber qual será o próximo
+                <span className="text-green-400 mr-1">
+                  projeto
+                </span>
+                ?
+              </header>
+              <SearchCode className="absolute element-to-move text-zinc-500 " size={100} />
+              <p className="text-zinc-50 text-center text-lg font-bold w-4/5">
+                Então, me acompanhe nas redes sociais e não perca nenhuma novidade!
+              </p>
+              <div className="flex justify-end gap-5 text-zinc-50">
+                <ListSocialMediaIcon iconSize={25} />
+              </div>
+            </li>
+
           </ul>
 
         </div>{/* end cards*/}
