1201f97a61a0784504e487160e08a9ba262b1699
charles-mrt <charlesmartins.ads@gmail.com>
2024-05-23 11:58:56 -0300

    feat: import contactForm component

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,5 +1,6 @@
-import { ListSocialMediaIcon } from "./SocialIcons/ListSocialMediaIcon"
-import { XCircle } from 'lucide-react'
+import { ContactForm } from './ContactForm';
+import { ListSocialMediaIcon } from './SocialIcons/ListSocialMediaIcon'
+import {  XCircle } from 'lucide-react'
 
 interface ModalProps {
   display: string;
@@ -11,22 +12,31 @@ export const Modal = ({ display, onClose }: ModalProps) => {
   return (
 
     <div className={`w-full h-full bg-black bg-opacity-90 ${display} top-0 bottom-0 right-0 left-0 m-auto z-[9999] flex items-center justify-center`}>
-      
-      <div className="relative max-w-lg h-1/2 flex flex-col items-center justify-around bg-zinc-800 border-2 border-green-400 rounded-lg p-10">
-        <a onClick={onClose}>
-          <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
-        </a>
 
-        <p className="text-sm text-center font-semibold text-zinc-50 m-5 laptop:text-2xl">
-          Fique à vontade para me contatar pelas redes sociais,<br></br> será um prazer receber sua mensagem!
-        </p>
+      <div className="relative max-w-lg  bg-zinc-800 border-2 border-green-400 rounded-lg p-10">
+        <header>
+          <a onClick={onClose}>
+            <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
+          </a>
+          <p className="text-center text-slate-50 text-2xl mb-6">Envie-me uma mensagem,</p>
+        </header>
 
-        <div className="text-zinc-50 grid grid-cols-2 gap-2 laptop:flex laptop:gap-6">
-          <ListSocialMediaIcon iconSize={30} selector="border-2 border-green-400 rounded-lg  w-14 h-14 laptop:w-auto laptop:h-auto laptop:p-6" />
+        <div className="w-full pb-6">
+          <ContactForm />
         </div>
-      </div>
 
-    </div>
+        <footer className='bg-zinc-900 p-2 rounded-md'>
+          <p className="text-sm text-center font-semibold text-zinc-50 p-2 laptop:text-xl">
+            Ou<br></br> fique à vontade para me contatar pelas redes sociais,<br></br> será um prazer receber sua mensagem!
+          </p>
+
+          <div className="text-zinc-50 flex gap-2 justify-center laptop:flex laptop:justify-between">
+            <ListSocialMediaIcon iconSize={30} selector="border-2 border-green-400 rounded-lg  w-14 h-14 laptop:w-auto laptop:h-auto laptop:p-6" />
+          </div>
+        </footer>
+
+      </div>
+    </div >
 
   )
 }
