b14b62aece1d16930459116a6840091a687070cf
charles-mrt <charlesmartins.ads@gmail.com>
2024-05-24 17:18:47 -0300

    style: Adjust styling for mobile devices

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,6 +1,6 @@
 import { ContactForm } from './ContactForm';
 import { ListSocialMediaIcon } from './SocialIcons/ListSocialMediaIcon'
-import {  XCircle } from 'lucide-react'
+import { XCircle } from 'lucide-react'
 
 interface ModalProps {
   display: string;
@@ -18,7 +18,7 @@ export const Modal = ({ display, onClose }: ModalProps) => {
           <a onClick={onClose}>
             <XCircle className="text-zinc-50 absolute top-0 right-0 mt-2 mr-2 :hover cursor-pointer" />
           </a>
-          <p className="text-center text-slate-50 text-xl mb-6">Envie-me uma mensagem,</p>
+          <p className="mb-1 mt-4 text-base text-center text-slate-50  laptop:text-xl laptop:mb-6 laptop:mt-0">Envie-me uma mensagem</p>
         </header>
 
         <div className="w-full pb-6">
@@ -26,12 +26,12 @@ export const Modal = ({ display, onClose }: ModalProps) => {
         </div>
 
         <footer className='bg-zinc-900 p-2 rounded-md'>
-          <p className="text-sm text-center text-zinc-50 p-2 laptop:text-xl">
-          <strong>Ou</strong><br></br> fique à vontade para me contatar pelas redes sociais,<br></br> será um prazer receber sua mensagem!
+          <p className="hidden laptop:block text-sm text-center text-zinc-50 p-2 laptop:text-xl">
+            <strong>Ou</strong><br></br> fique à vontade para me contatar pelas redes sociais,<br></br> será um prazer receber sua mensagem!
           </p>
 
           <div className="text-zinc-50 flex gap-2 justify-center laptop:flex laptop:justify-between">
-            <ListSocialMediaIcon iconSize={30} selector="border-2 border-green-400 rounded-lg  w-14 h-14 laptop:w-auto laptop:h-auto laptop:p-6" />
+            <ListSocialMediaIcon iconSize={30} selector="border-2 border-green-400 rounded-lg  w-12 h-12 laptop:w-auto laptop:h-auto laptop:p-6" />
           </div>
         </footer>
 
