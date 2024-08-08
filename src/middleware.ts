ba990e2502011ddbb394cbeb4c93d4dc47be291c
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-08 11:22:56 -0300

    feat: add authentication middleware for route protection

---
+++ src/middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { isSessionValid } from './app/services/user'
+
+export const config = {
+  matcher: '/((?!_next/static|_next/image|favicon.ico).*)',
+}
+
+const publicRoutes = ['/', '/login']
+const privateRoutesForLoggedIn = ['/login']
+
+export async function middleware(req: NextRequest) {
+  const session = await isSessionValid()
+  const pathname = req.nextUrl.pathname
+
+  if (publicRoutes.includes(pathname)) {
+    if (!session) return NextResponse.next()
+  }
+
+  
+  if (privateRoutesForLoggedIn.includes(pathname)) {
+    if (session) return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  // Check if the user is logged in for non-public routes
+  if (!session) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
+
+  return NextResponse.next()
+}
