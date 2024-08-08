90adc83b66aef11dfa22fdd92411b686b9982db1
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-08 11:26:24 -0300

    feat: menu for logged in users

--- src/app/components/Menu/LoggedInMenu.tsx
+++ src/app/components/Menu/LoggedInMenu.tsx
@@ -1,6 +1,6 @@
-import { ReactNode } from "react"
-import Link from "next/link"
-import { HomeIcon, LayoutDashboard, LogOut } from "lucide-react"
+import { ReactNode } from 'react'
+import Link from 'next/link'
+import { HomeIcon, LayoutDashboard, LogOut } from 'lucide-react'
 
 const navMenu = {
   home: {
@@ -18,15 +18,16 @@ const navMenu = {
 };
 
 interface MenuItemProps {
-  route: string;
-  children: ReactNode;
+  route: string
+  children: ReactNode
+  background: string 
 }
 
-const MenuItem = ({ route, children }: MenuItemProps) => {
+const MenuItem = ({ route, children, background}: MenuItemProps) => {
   return (
     <Link
       href={route}
-      className="flex gap-2 items-center justify-center bg-zinc-700 p-2 rounded-md text-zinc-100 font-sans capitalize"
+      className={`flex gap-2 items-center justify-center p-2 rounded-md text-zinc-100 font-sans capitalize ${background}`}
     >
       {children}
     </Link>
@@ -38,19 +39,18 @@ export const LoggedInMenu = () => {
     <header className="fixed z-50 top-0 left-0 w-full h-auto p-2 border-b-[1px] bg-zinc-800 border-green-500">
       <nav className="flex justify-between">
         <div className="flex gap-2">
-          <MenuItem route={navMenu.home.route}>
+          <MenuItem route={navMenu.home.route} background={'bg-zinc-700'}>
             <HomeIcon width={16} />
-            {navMenu.home.name}
-            teste
+            {navMenu.home.name}          
           </MenuItem>
 
-          <MenuItem route={navMenu.dashboard.route}>
+          <MenuItem route={navMenu.dashboard.route} background="bg-green-400">
             <LayoutDashboard width={16} />
             {navMenu.dashboard.name}
           </MenuItem>
         </div>
 
-        <MenuItem route={navMenu.logout.route}>
+        <MenuItem route={navMenu.logout.route} background={'bg-red-400/80'}>
           <LogOut width={16} />
           {navMenu.logout.name}
         </MenuItem>
