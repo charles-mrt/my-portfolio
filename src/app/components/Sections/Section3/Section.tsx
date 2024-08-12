30ff065c23a141b5c224ec3c063d0ccca2e4337a
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-12 12:08:34 -0300

    refactor: separate loading logic with Suspense API and use skeleton loader

--- src/app/components/Sections/Section3/Section.tsx
+++ src/app/components/Sections/Section3/Section.tsx
@@ -1,21 +1,24 @@
-'use client'
+
 
 import { ProjectCards } from '../../Projects/ProjectCards'
-import { SectionTitle } from '../../SectionTitle';
+import { SectionTitle } from '../../SectionTitle'
 
-import { projectsData } from '@/app/data';
-import { SearchProject } from '../../SearchProject';
-import { useState } from 'react';
-import { SearchCode, TerminalSquare } from 'lucide-react';
-import { ListSocialMediaIcon } from '../../SocialIcons/ListSocialMediaIcon';
+import { projectsData } from '@/app/data'
+import { SearchProject } from '../../SearchProject'
+import { Suspense, useState } from 'react'
+import { SearchCode, TerminalSquare } from 'lucide-react'
+import { ListSocialMediaIcon } from '../../SocialIcons/ListSocialMediaIcon'
+import { PublicProjects } from '../../Projects/PublicProjects'
+import { ProjectCardSkeleton } from '../../Projects/ProjectCardSkeleton'
 
 export const Section = () => {
-  const [selectedCategory, setSelectedCategory] = useState("todas");
+  // const [selectedCategory, setSelectedCategory] = useState("todas")
 
-  const handleCategoryChange = (category: string) => {
-    setSelectedCategory(category.trim());
-  };
-  
+  // const handleCategoryChange = (category: string) => {
+  //   setSelectedCategory(category.trim())
+  // }
+
+
   return (
 
     < section
@@ -28,59 +31,14 @@ export const Section = () => {
           <SectionTitle titleName1="projetos" titleName2="realizados." />
         </header>
 
-        <SearchProject onCategoryChange={handleCategoryChange} />
-
-        {/*cards */}
         <div className="w-full">
+          <Suspense fallback={ 
+            <ProjectCardSkeleton totalCards={3} />
+          }>
+            <PublicProjects />
+          </Suspense>
 
-          <ul className="grid justify-items-center gap-4 sm:grid-cols-2 laptop:grid-cols-3">
-            {
-              projectsData.map((project, index) => {
-                return (
-                  project.is_public && project.categories.includes(selectedCategory) ? (
-                    <li key={index}>
-                      <ProjectCards
-                        imageUrl={project.image}
-                        alt={project.alt}
-                        title={project.title}
-                        technologiesListImage={project.tecnologies}
-                        description={project.description}
-                        tec_description={project.tec_description}
-                        github_url={project.github}
-                        project_url={project.url}
-                        category={project.categories}
-                      />
-                    </li>
-                  ) : <></>
-                )
-              })
-            }
-
-            <li className="h-[31.2rem] w-80 sm-device:w-72 sm:w-[19rem] md:w-[22rem] lg:w-[19.5rem] xl:w-96 2xl:h-[40.6rem] 
-            bg-zinc-900 p-10 border-2 border-dashed border-green-400 rounded-[5px] flex flex-col justify-around items-center">
-
-              <header className="text-2xl text-center text-zinc-50">
-                Curioso para saber qual será o próximo
-                <span className="text-green-400 ml-2 mr-1">
-                  projeto
-                </span>
-                ?
-              </header>
-              <div className="relative w-28 h-28 bg-zinc-800 rounded-md flex items-center justify-center">
-                <TerminalSquare className="text-green-300" strokeWidth={1} size={100} />
-                <SearchCode className="absolute element-to-move text-zinc-500 " size={100} />
-              </div>
-              <p className="text-zinc-50 text-center text-lg font-bold w-4/5">
-                Então, me acompanhe nas redes sociais e não perca nenhuma novidade!
-              </p>
-              <div className="flex justify-end gap-5 text-zinc-50">
-                <ListSocialMediaIcon iconSize={25} />
-              </div>
-            </li>
-
-          </ul>
-
-        </div>{/* end cards*/}
+        </div>
 
       </div >
 
