8c4536e38f210943b8f5f4ab093b3d0483c00105
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-12 16:45:55 -0300

    style: animation avatar

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import google_plus from '/public/assets/google-plus.svg'
-import avatar from '/public/assets/avatar.png'
+import avatar from '/public/assets/avatar.webp'
 
 
 import { useSearchParams } from 'next/navigation'
@@ -16,14 +16,20 @@ export default function Login() {
   return (
     <section className="flex justify-center items-center w-full h-full p-10">
 
-      <div className={`relative bg-zinc-950  w-96 h-80 rounded-md border ${errorMessage ? 'border-red-500 shadow-red-500' : 'border-green-400 shadow-green-400'}  shadow-custom  text-zinc-300 p-6 `}>
-
-        <div className="absolute top-0 left-0 right-0 m-auto -translate-y-2/3 w-32 h-32 my-5 overflow-hidden rounded-full border-4 border-zinc-800 bg-zinc-950 p-2">
-          <Image
-            src={avatar}
-            alt="imagem do Charles"
-          />
-        </div>
+      <div className={`relative bg-zinc-950  w-96 h-80 rounded-md border ${errorMessage ? 'border-red-500 shadow-red-500' : 'border-green-400 shadow-green-400'} border-t-0  shadow-custom  text-zinc-300 p-6 `}>
+       
+        <div className="absolute z-0 top-0 left-0 right-0 m-auto w-44 h-20 bg-zinc-950"/>
+          <div className="absolute top-0 left-0 right-0 m-auto -translate-y-2/3  w-44 h-44 my-5 ">
+            <div className="absolute z-10 inset-0 m-auto rounded-full w-40 h-40 border-r-4 border-b-4 border-dotted border-green-400 animate-spinClockwise" />
+            <div className="absolute -z-0 inset-0 m-auto rounded-full w-36 h-36 bg-gradient-to-r from-zinc-900 via-violet-700 to-green-500 animate-spinCounterclockwise" />
+            <div className="absolute z-20 inset-0 m-auto rounded-full w-44 h-44 border-r-4 border-b-4 border-dotted border-zinc-400 animate-spinCounterclockwise" />
+            <Image
+              className="absolute z-10 inset-0 m-auto rounded-full w-44 h-44 border-b-2 border-purple-600 "
+              src={avatar}
+              alt="imagem do Charles"
+            />
+          </div>
+        {/* </div> */}
 
         <div className="mt-14 text-center flex flex-col items-center gap-6">
           <h1 className="font-bold">Bem vindo ao portfolio de Charles Martins</h1>
