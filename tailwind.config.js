7236fd3a2792e00b5f7cecd5bbb227ed85f2a739
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-12 16:21:39 -0300

    style: config custom keyframe

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,7 +26,37 @@ module.exports = {
       backgroundImage: {
         'section-1':"url('/assets/bg-section-1.jpg')",
         'background': "url('/assets/background.jpg')",
-      }
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        spinClockwise: {
+          '0%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(360deg)' },
+          '50%': { transform: 'rotate(0deg)' },
+          '75%': { transform: 'rotate(-360deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+        spinCounterclockwise: {
+          '0%': { transform: 'rotate(0deg)' },
+          '25%': { transform: 'rotate(-360deg)' },
+          '50%': { transform: 'rotate(0deg)' },
+          '75%': { transform: 'rotate(360deg)' },
+          '100%': { transform: 'rotate(0deg)' },
+        },
+      },      
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "spinClockwise": "spinClockwise 50s linear infinite",
+        "spinCounterclockwise": "spinCounterclockwise 40s linear infinite",
+      },
     },
   },
   plugins: [
