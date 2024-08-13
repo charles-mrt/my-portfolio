c39fe2a2db97ebb207331376388e30a338759494
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-13 17:52:27 -0300

    feat: update portfolio about me section

--- src/app/components/Sections/Section2/Section.tsx
+++ src/app/components/Sections/Section2/Section.tsx
@@ -39,7 +39,7 @@ export const Section = () => {
   const [isToogle, setIsToogle] = useState(Boolean)
 
   const handleToogle = () => setIsToogle(!isToogle)
-  
+
   return (
     <section
       id="about-me"
@@ -54,7 +54,7 @@ export const Section = () => {
           </header>
 
           <div className={` ${!isToogle ? 'text-clip overflow-hidden transition-about-close ' : 'overflow-clip transition-about-open '
-            } font-normal text-lg text-center tracking-[1px] text-zinc-50 laptop:text-left lg:indent-8`}>
+            } font-normal text-lg text-center tracking-[1px] text-zinc-50 laptop:text-left lg:indent-8 space-y-2`}>
 
             <p>Iniciei minha jornada na programação guiado pela curiosidade de compreender o funcionamento por trás das interfaces de computadores. Ao longo desse percurso, obtive minha formação em Análise e Desenvolvimento de Sistemas e também me graduei em Gestão da Tecnologia da Informação.</p>
             <p>Desde então, tenho dedicado tempo e esforço ao aprimoramento das minhas habilidades em desenvolvimento de software.</p>
@@ -62,9 +62,20 @@ export const Section = () => {
             <p>Minha abordagem no desenvolvimento web está centrada na usabilidade, no design e na performance. Meu objetivo é criar interfaces amigáveis e acessíveis, proporcionando uma excelente experiência ao usuário em diversos dispositivos.</p>
             <p>Estou constantemente em busca de novos desafios e oportunidades para ampliar meu conhecimento e habilidades como desenvolvedor frontend. Minha paixão pela programação e meu compromisso com a aprendizagem contínua me motivam a criar soluções web eficazes e inovadoras.</p>
             <p>Além disso, tenho um genuíno interesse em compartilhar conhecimento com a comunidade. Planejo criar conteúdo educativo, como vídeo aulas, para ajudar outros desenvolvedores a aprimorar suas habilidades e crescer na área.</p>
-
+            <p className="pt-4">Sou também o criador da <strong>Sala do Café.dev</strong>,            
+              uma marca dedicada a camisetas especialmente para desenvolvedores e amantes de café. A marca surgiu da minha paixão por programação e pelo café, e oferece designs únicos que combinam humor, estilo e o amor por ambas as paixões.</p>
+            <p>Cada peça é criada com o objetivo de celebrar a cultura do desenvolvimento e o prazer de uma boa xícara de café, proporcionando aos nossos clientes uma forma divertida e original de expressar sua identidade.</p>
+            <p>Para conferir nossas coleções e encontrar a camiseta perfeita, visite a
+              <a
+                href="https://reserva.ink/saladocafedev"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-400 font-bold  px-2"
+              >
+                Sala do Café.dev
+              </a></p>
           </div>
-          
+
           <button
             className="text-green-400"
             onClick={() => handleToogle()}
