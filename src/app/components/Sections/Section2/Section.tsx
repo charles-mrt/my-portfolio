f3254001917ac129b2fe97bb3b377b444a580a04
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-14 17:17:59 -0300

    style: fix text color when accordion is open

--- src/app/components/Sections/Section2/Section.tsx
+++ src/app/components/Sections/Section2/Section.tsx
@@ -53,8 +53,8 @@ export const Section = () => {
             <SectionTitle titleName1="sobre" titleName2="mim." />
           </header>
 
-          <div className={` ${!isToogle ? 'bg-gradient-to-b from-zinc-50 via-zinc-50 to-zinc-800 text-transparent bg-clip-text overflow-hidden transition-about-close ' : 'overflow-clip transition-about-open '
-            } font-normal text-lg text-center tracking-[1px] text-zinc-50 laptop:text-left lg:indent-8 space-y-2 `}>
+          <div className={` ${!isToogle ? 'bg-gradient-to-b from-zinc-50 via-zinc-50 to-zinc-800 text-transparent bg-clip-text overflow-hidden transition-about-close ' : 'overflow-clip transition-about-open text-zinc-50'
+            } font-normal text-lg text-center tracking-[1px] laptop:text-left lg:indent-8 space-y-2`}>
 
             <p>Iniciei minha jornada na programação guiado pela curiosidade de compreender o funcionamento por trás das interfaces de computadores. Ao longo desse percurso, obtive minha formação em Análise e Desenvolvimento de Sistemas e também me graduei em Gestão da Tecnologia da Informação.</p>
             <p>Desde então, tenho dedicado tempo e esforço ao aprimoramento das minhas habilidades em desenvolvimento de software.</p>
@@ -62,7 +62,7 @@ export const Section = () => {
             <p>Minha abordagem no desenvolvimento web está centrada na usabilidade, no design e na performance. Meu objetivo é criar interfaces amigáveis e acessíveis, proporcionando uma excelente experiência ao usuário em diversos dispositivos.</p>
             <p>Estou constantemente em busca de novos desafios e oportunidades para ampliar meu conhecimento e habilidades como desenvolvedor frontend. Minha paixão pela programação e meu compromisso com a aprendizagem contínua me motivam a criar soluções web eficazes e inovadoras.</p>
             <p>Além disso, tenho um genuíno interesse em compartilhar conhecimento com a comunidade. Planejo criar conteúdo educativo, como vídeo aulas, para ajudar outros desenvolvedores a aprimorar suas habilidades e crescer na área.</p>
-            <p className="pt-4">Sou também o criador da <strong>Sala do Café.dev</strong>,            
+            <p className="pt-4">Sou também o criador da <strong>Sala do Café.dev</strong>,
               uma marca dedicada a camisetas especialmente para desenvolvedores e amantes de café. A marca surgiu da minha paixão por programação e pelo café, e oferece designs únicos que combinam humor, estilo e o amor por ambas as paixões.</p>
             <p>Cada peça é criada com o objetivo de celebrar a cultura do desenvolvimento e o prazer de uma boa xícara de café, proporcionando aos nossos clientes uma forma divertida e original de expressar sua identidade.</p>
             <p>Para conferir nossas coleções e encontrar a camiseta perfeita, visite a
