974a196715bb25e6602a59b8a9d0d8594b3f8d68
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-14 16:29:28 -0300

    feat: Update category label 'all' to 'todas' in UI

--- src/app/components/SearchProject.tsx
+++ src/app/components/SearchProject.tsx
@@ -3,7 +3,7 @@ import { Search } from 'lucide-react'
 import { useState } from 'react'
 
 const categories = [
-  'todas',
+  'all',
   'javascript',
   'reactjs',
   'react-native',
@@ -52,16 +52,19 @@ export const SearchProject = ({ onCategoryChange }: SearchProjectProps) => {
 
       <ul className='w-11/12 m-auto flex items-center justify-center flex-wrap gap-2 mb-2 custom-fade-animation' translate='no'>
         {searchByCategory && (
-          categories.map((category, index) => (
-            <li
-              key={index}
-              className={`${selectedCategory === category ? 'bg-zinc-700' : 'bg-transparent'} border-2 border-transparent rounded-md p-1 text-sm hover:border-green-400 cursor-pointer transition`}
-              data-category={category.trim()}
-              onClick={() => onClickHandle(category)}
-            >
-              {category}
-            </li>
-          ))
+          categories.map((category, index) => {
+            const displayText = category === 'all' ? 'todas' : category
+            return (
+              <li
+                key={index}
+                className={`${selectedCategory === category ? 'bg-zinc-700' : 'bg-transparent'} border-2 border-transparent rounded-md p-1 text-sm hover:border-green-400 cursor-pointer transition`}
+                data-category={category}
+                onClick={() => onClickHandle(category)}
+              >
+                {displayText}
+              </li>
+            );
+          })
         )}
       </ul>
     </div>
