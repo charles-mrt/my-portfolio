3749ab89a10d63380ea4c44f7ca746ea1f19a212
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-14 16:35:03 -0300

    feat: prevent UI replacement by fallback during updates

--- src/app/components/Sections/Section3/Section.tsx
+++ src/app/components/Sections/Section3/Section.tsx
@@ -1,47 +1,39 @@
 
-
-import { ProjectCards } from '../../Projects/ProjectCards'
+'use client'
 import { SectionTitle } from '../../SectionTitle'
-
-import { projectsData } from '@/app/data'
-import { SearchProject } from '../../SearchProject'
-import { Suspense, useState } from 'react'
-import { SearchCode, TerminalSquare } from 'lucide-react'
-import { ListSocialMediaIcon } from '../../SocialIcons/ListSocialMediaIcon'
+import { Suspense, useState, startTransition} from 'react'
 import { PublicProjects } from '../../Projects/PublicProjects'
 import { ProjectCardSkeleton } from '../../Projects/ProjectCardSkeleton'
+import { SearchProject } from '../../SearchProject'
 
 export const Section = () => {
-  // const [selectedCategory, setSelectedCategory] = useState("todas")
+  
+  const [selectedCategory, setSelectedCategory] = useState("all")
 
-  // const handleCategoryChange = (category: string) => {
-  //   setSelectedCategory(category.trim())
-  // }
-
-
+  const handleCategoryChange = (category: string) => {
+    startTransition(() => {
+      setSelectedCategory(category.trim())
+    })
+  }
   return (
 
-    < section
-      id="project"
-      className="w-full h-auto bg-zinc-900 py-10 laptop:bg-opacity-90"
-    >
+    <section id="project" className="w-full h-auto bg-zinc-900 py-10 laptop:bg-opacity-90">
       <div className="w-full max-w-7xl m-auto">
-
+        
         <header className="text-center mt-10 mb-5">
           <SectionTitle titleName1="projetos" titleName2="realizados." />
         </header>
 
         <div className="w-full">
+          <SearchProject onCategoryChange={handleCategoryChange} />
           <Suspense fallback={ 
             <ProjectCardSkeleton totalCards={3} />
           }>
-            <PublicProjects />
+            <PublicProjects category={selectedCategory}/>
           </Suspense>
-
         </div>
 
-      </div >
-
-    </section >
+      </div>
+    </section>
   )
 }
