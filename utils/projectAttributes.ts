404a40bbea865d55540f0d7fbee3d7bf9e03df42
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-15 16:32:18 -0300

    feat: project technologies and categories data

---
+++ utils/projectAttributes.ts
@@ -0,0 +1,43 @@
+
+// Project tecnologies
+export const TECHNOLOGIES: string[] = [
+  'javascript',
+  'react-js',
+  'react-native',
+  'next-js',
+  'node-js',
+  'typescript',
+  'php',
+  'socketio',
+  'wordpress',
+  'css',
+  'css-module',
+  'sass',
+  'styled-component',
+  'tailwind',
+  'html',
+  'figma',
+  'mongodb',
+  'fastify'
+];
+
+// Project categories
+export const CATEGORIES: string[] = [
+  "all",
+  "javascript",
+  "react-js",
+  "react-native",
+  "next-js",
+  "node-js",
+  "typescript",
+  "php",
+  "wordpress",
+  "css",
+  "css-modules",
+  "sass",
+  "tailwind",
+  "html",
+  "game",
+  "figma",
+  "code-art",
+];
