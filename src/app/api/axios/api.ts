f0b14117e3b79520c449547c7dfa8e3cd6ef84e4
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-15 17:08:14 -0300

    fix: rename environment variable

--- src/app/api/axios/api.ts
+++ src/app/api/axios/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_API_URL }) 
+const api = axios.create({ baseURL: process.env.API_URL }) 
 
 export default api
