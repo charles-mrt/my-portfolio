cdee7d9a609c3b4778dca152bc06ec300b587dbd
charles-mrt <charlesmartins.ads@gmail.com>
2024-08-15 16:29:16 -0300

    style: resize avatar image

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -20,11 +20,11 @@ export default function Login() {
        
         <div className="absolute z-0 top-0 left-0 right-0 m-auto w-44 h-20 bg-zinc-950"/>
           <div className="absolute top-0 left-0 right-0 m-auto -translate-y-2/3  w-44 h-44 my-5 ">
-            <div className="absolute z-10 inset-0 m-auto rounded-full w-40 h-40 border-r-4 border-b-4 border-dotted border-green-400 animate-spinClockwise" />
-            <div className="absolute -z-0 inset-0 m-auto rounded-full w-36 h-36 bg-gradient-to-r from-zinc-900 via-violet-700 to-green-500 animate-spinCounterclockwise" />
-            <div className="absolute z-20 inset-0 m-auto rounded-full w-44 h-44 border-r-4 border-b-4 border-dotted border-zinc-400 animate-spinCounterclockwise" />
+            <div className="absolute z-10 inset-0 m-auto rounded-full w-36 h-36 border-r-4 border-b-4 border-dotted border-green-400 animate-spinClockwise" />
+            <div className="absolute -z-0 inset-0 m-auto rounded-full w-32 h-32 bg-gradient-to-r from-zinc-900 via-violet-700 to-green-500 animate-spinCounterclockwise" />
+            <div className="absolute z-20 inset-0 m-auto rounded-full w-40 h-40 border-r-4 border-b-4 border-dotted border-zinc-400 animate-spinCounterclockwise" />
             <Image
-              className="absolute z-10 inset-0 m-auto rounded-full w-44 h-44 border-b-2 border-purple-600 "
+              className="absolute z-10 inset-0 m-auto rounded-full w-36 h-36 border-b-2 border-purple-600"
               src={avatar}
               alt="imagem do Charles"
             />
